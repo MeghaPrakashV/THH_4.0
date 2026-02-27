cc3a7314e4da107c29a2450cf41b2ce62b3ef2f4
Megha <meghaprakashv@gmail.com>
2026-02-28 00:36:07 +0530

    pre-rebase commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyBMz3ci3jMNJqzyWGWlUtOSS93O-0Dv6VA",
+  authDomain: "hostel-survival-kit.firebaseapp.com",
+  projectId: "hostel-survival-kit",
+  storageBucket: "hostel-survival-kit.firebasestorage.app",
+  messagingSenderId: "453209875257",
+  appId: "1:453209875257:web:a7e0fd1f5eb17ef3847ee8"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
